1d6969e4eae44bc21811b6ed7826107087daf33e
few2net <few2net@gmail.com>
2021-03-23 18:06:29 +0700

    Minor change - Create env.bash - Edit example_0

---
+++ env.bash
@@ -0,0 +1,3 @@
+source /opt/ros/kinetic/setup.bash
+export ROS_MASTER_URI=http://192.168.1.173:11311
+export ROS_IP=192.168.1.98
--- example_0_First_program.py
+++ example_0_First_program.py
@@ -2,5 +2,6 @@ from api import Turtlebot
 import time
 
 robot = Turtlebot()
+print("Hello world!")
 while(True):
 	robot.move_robot(10,10)
